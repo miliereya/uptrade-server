d4bd5635c304adb0ffddebd0c7f545f90b7ed265
miliereya <105806526+miliereya@users.noreply.github.com>
2022-11-29 20:54:45 +0300

    Update user-controller.js

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -12,7 +12,7 @@ class UserController {
             const { email, password } = req.body
             const userData = await userService.registr(email, password)
 
-            res.cookie('refreshToken', userData.refreshToken, { maxAge: 30 * 24 * 60 * 60 * 1000})
+            res.cookie('refreshToken', userData.refreshToken, { maxAge: 30 * 24 * 60 * 60 * 1000, sameSite: 'none'})
 
             return res.json(userData)
         } catch (e) {
@@ -23,7 +23,7 @@ class UserController {
         try {
             const { email, password } = req.body
             const userData = await userService.login(email, password)
-            res.cookie('refreshToken', userData.refreshToken, { maxAge: 30 * 24 * 60 * 60 * 1000 })
+            res.cookie('refreshToken', userData.refreshToken, { maxAge: 30 * 24 * 60 * 60 * 1000, sameSite: 'none' })
 
             return res.json(userData)
         } catch (e) {
@@ -44,7 +44,7 @@ class UserController {
         try {
             const { refreshToken } = req.cookies
             const userData = await userService.refresh(refreshToken)
-            res.cookie('refreshToken', userData.refreshToken, { maxAge: 30 * 24 * 60 * 60 * 1000})
+            res.cookie('refreshToken', userData.refreshToken, { maxAge: 30 * 24 * 60 * 60 * 1000, sameSite: 'none'})
             return res.json(userData)
         } catch (e) {
             next(e)
