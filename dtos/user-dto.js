27c486e78e7cb593e350d797a7a9491ba2d40719
miliereya <105806526+miliereya@users.noreply.github.com>
2022-11-29 20:16:27 +0300

    updated userDto
    added id atribute

--- dtos/user-dto.js
+++ dtos/user-dto.js
@@ -1,8 +1,10 @@
 module.exports = class UserDto {
+    id
     email
     projects
 
     constructor(model) {
+        this.id = model._id
         this.email = model.email
         this.projects = model.projects
     }
