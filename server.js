09cecace9737d834e7e641c976188e6640bed878
miliereya <105806526+miliereya@users.noreply.github.com>
2022-12-04 19:22:38 +0300

    Update server.js

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ const app = express()
 const port = process.env.PORT || 5000
 
 app.use(cors({
-    origin: '*',
+    origin: process.env.CLIENT_URL,
     credentials: true
 }))
 app.use(express.json())
